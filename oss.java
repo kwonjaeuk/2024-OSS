2d682bea1ae6c5e17b6edca678ff6b11e4e293a8
Jaeuk Kwon <snust21itm@gmail.com>
2025-05-01 10:17:19 +0900

    Create oss.java

---
+++ oss.java
@@ -0,0 +1,6 @@
+// Dummy Java file to trigger language detection
+public class OSS {
+    public static void main(String[] args) {
+        System.out.println("This is a dummy file for GitHub language stats.");
+    }
+}
